00f5ce5c9f85e1571b733193475d47ce0dc66ff9
djchinia <djchinia@gmail.com>
2019-09-04 17:00:13 -0400

    some small changes

---
+++ client/components/ImageUploadPage.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import ImageUploader from 'react-images-upload'
+
+class ImageUploadPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pictures: []
+    }
+    this.onDrop = this.onDrop.bind(this)
+  }
+
+  onDrop(picture) {
+    this.setState({
+      pictures: this.state.pictures.concat(picture)
+    })
+  }
+
+  render() {
+    return (
+      <div className="imageUploader">
+        <ImageUploader
+          withIcon={true}
+          buttonText="Choose images"
+          onChange={this.onDrop}
+          imgExtension={['.jpg', '.gif', '.png', '.gif']}
+          maxFileSize={5242880}
+        />
+      </div>
+    )
+  }
+}
+
+export default ImageUploadPage
--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import store from './store'
 import App from './app'
 import 'semantic-ui-css/semantic.min.css'
 
+// https://www.youtube.com/watch?v=d1RG9MeC47k&list=PLR2qQy0Zxs_UpmxF0qqorwYcSQFcRCuHo&index=3
 // establishes socket connection
 import './socket'
 
