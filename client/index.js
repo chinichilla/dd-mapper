200ab126f4eea5145ca3d79f766637f809cbdaab
djchinia <djchinia@gmail.com>
2019-09-08 19:19:34 -0400

    restructure routes and add dndrop

--- client/index.js
+++ client/index.js
@@ -5,17 +5,20 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
-import 'semantic-ui-css/semantic.min.css'
+import {DndProvider} from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 // https://www.youtube.com/watch?v=d1RG9MeC47k&list=PLR2qQy0Zxs_UpmxF0qqorwYcSQFcRCuHo&index=3
 // establishes socket connection
 import './socket'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  <DndProvider backend={HTML5Backend}>
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  </DndProvider>,
   document.getElementById('app')
 )
