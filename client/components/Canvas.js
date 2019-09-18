ebc249a87cc584c9737fcd288264421e0f66495d
djchinia <djchinia@gmail.com>
2019-09-18 18:45:18 -0400

    render one square for canvas grid

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -1,19 +1,51 @@
 import React, {Component} from 'react'
 import '../styles/index.css'
 
-class Canvas extends Component {
+export default class Canvas extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      squareSize: 20,
+      canvasSize: {canvasWidth: 800, canvasHeight: 600}
+    }
+  }
+
+  componentDidMount() {
+    const {canvasWidth, canvasHeight} = this.state.canvasSize
+    this.canvasSquare.width = canvasWidth
+    this.canvasSquare.height = canvasHeight
+    this.drawSquare(this.canvasSquare, {x: 50, y: 50})
+  }
+
+  drawSquare(canvasID, coordinates) {
+    const ctx = canvasID.getContext('2d')
+    ctx.beginPath()
+    ctx.rect(
+      coordinates.x,
+      coordinates.y,
+      this.state.squareSize,
+      this.state.squareSize
+    )
+    ctx.stroke()
+    ctx.closePath()
   }
 
   render() {
+    console.log(this.props.mapImage.imageUrl)
     return (
       <div>
-        <canvas />
+        <canvas
+          ref={canvasSquare => (this.canvasSquare = canvasSquare)}
+          width={this.state.canvasWidth}
+          height={this.state.canvasHeight}
+        >
+          {/* <img
+            src={this.props.mapImage.imageUrl}
+            width={this.state.canvasWidth}
+            height={this.state.canvsHeight}
+          /> */}
+        </canvas>
       </div>
     )
   }
 }
-
-export default Canvas
