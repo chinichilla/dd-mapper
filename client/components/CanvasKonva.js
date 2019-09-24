b4691d20e0eaab897346e90fe34284fcb4df15ac
djchinia <djchinia@gmail.com>
2019-09-24 00:30:44 -0400

    map with grid using konva

--- client/components/CanvasKonva.js
+++ client/components/CanvasKonva.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import {Stage, Layer, Text} from 'react-konva'
+import {Stage, Layer, Text, Rect, Line} from 'react-konva'
 import URLImage from './URLImage'
+import Grid from './Grid'
 
 export class CanvasKonva extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isDragging: false,
-      x: 0,
-      y: 0
+      player1: {x: 0, y: 10},
+      player2: {x: 0, y: 30},
+      gridLength: 50
     }
   }
 
@@ -19,10 +21,38 @@ export class CanvasKonva extends Component {
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <URLImage src={imageUrl} x={0} y={0} />
+          <Grid gridLength={this.state.gridLength} />
+          {/* <Rect
+            x={0}
+            y={0}
+            width={window.innerWidth}
+            height={window.innerHeight}
+            fill="gray"
+            opacity="0.75"
+          /> */}
           <Text
-            text="Draggable Text"
-            x={this.state.x}
-            y={this.state.y}
+            text="Player 1"
+            x={this.state.player1.x}
+            y={this.state.player1.y}
+            draggable
+            fill={this.state.isDragging ? 'green' : 'black'}
+            onDragStart={() => {
+              this.setState({
+                isDragging: true
+              })
+            }}
+            onDragEnd={e => {
+              this.setState({
+                isDragging: false,
+                x: e.target.x(),
+                y: e.target.y()
+              })
+            }}
+          />
+          <Text
+            text="Player 2"
+            x={this.state.player2.x}
+            y={this.state.player2.y}
             draggable
             fill={this.state.isDragging ? 'green' : 'black'}
             onDragStart={() => {
